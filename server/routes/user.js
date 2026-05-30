51c1bfdcaf44be27853d70cec8a9d3770c028acb
Aman Kumar Sah <sah99017@gmail.com>
2026-05-30 22:06:20 +0545

    fix(routes): import mongoose in user routes for the health check endpoint

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
 
 const {login, signup} = require('../controllers/Auth')
 const { auth, isStudent, isAdmin } = require('../middlewares/auth')
